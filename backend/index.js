119c85b7c53d6a87e093f7d49fb648f07c78d340
MGautam-88 <malhotragautam46@gmail.com>
2026-06-10 01:02:44 +0530

    server check

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config(); // Load environment variables from .env file
 const connectDB = require('./config/db');
@@ -22,32 +23,31 @@ app.use(express.urlencoded({ extended: true }));
 
 // we can use rate limiter which will limit the number of requests from a single IP address in a given time frame to prevent abuse and DDoS attacks
 
-app.get("/", (req,res)=>{
-    res.send("ShopNest Backend is running");
-})
-
 app.use('/api/auth',userRoutes);//more clean way , we can use this again simply if wated without writing the whole path again!
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/payments', require('./routes/paymentRoutes'));
+app.use('/api/payment', require('./routes/paymentRoutes'));
 app.use('/api/analytics', adminRoutes);
+
+// Serve frontend build files
+if (process.env.NODE_ENV === 'production') {
+    const frontendBuildPath = path.join(__dirname, '../frontend/build');
+
+    app.use(express.static(frontendBuildPath));
+
+    // For any route not found in API, serve React app
+    app.use((req, res) => {
+        res.sendFile(path.join(frontendBuildPath, 'index.html'));
+    });
+} else {
+    app.get("/", (req,res)=>{
+        res.send("ShopNest Backend is running");
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-
-
-const path = require('path')
-
-// Serve frontend build files
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')))
-
-    // For any route not found in API, serve React app
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
-    })
-}
